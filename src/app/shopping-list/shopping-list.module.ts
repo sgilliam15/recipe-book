555fc19af794143d9ddf119aacb1181fdf8adb9e
sgilliam15 <stevengilliamjr@gmail.com>
2017-07-30 16:21:49 -0700

    -Make shopping list component available to app module

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -14,6 +14,9 @@ import { ShoppingListAddComponent } from './shopping-list-add/shopping-list-add.
   declarations: [
     ShoppingListComponent,
     ShoppingListAddComponent
+  ],
+  exports: [
+    ShoppingListComponent
   ]
 })
 export class ShoppingListModule { }
