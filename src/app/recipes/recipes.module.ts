a86d3b3d1e408f6f39e0412f3d1c11b34ff0771d
sgilliam15 <stevengilliamjr@gmail.com>
2017-08-18 22:21:48 -0700

    -rm uneeded import, shopping list service is available project wide

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// Custom Modules
-import { ShoppingListModule } from '../shopping-list/shopping-list.module';
-
 // Containers
 import { RecipesComponent } from './recipes.component';
 
@@ -13,15 +10,10 @@ import { RecipeItemComponent } from './recipe-list/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipesService } from './services/recipes.service';
 
-// Services
-import { ShoppingListService } from '../shopping-list/services/shopping-list.service';
-
-
 
 @NgModule({
   imports: [
-    CommonModule,
-    ShoppingListModule
+    CommonModule
   ],
   declarations: [
     RecipesComponent,
@@ -33,8 +25,7 @@ import { ShoppingListService } from '../shopping-list/services/shopping-list.ser
     RecipesComponent
   ],
   providers: [
-    RecipesService,
-    ShoppingListService
+    RecipesService
   ]
 })
 
